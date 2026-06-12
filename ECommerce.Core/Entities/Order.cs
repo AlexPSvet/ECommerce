2c4bacb35e30eaab8a998d9bf0d47e4c5877eb4d
AlexPSvet <alejandroperezsvet@gmail.com>
2026-06-12 12:03:40 -0600

    Base entities for the project.

---
+++ ECommerce.Core/Entities/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Core.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ ECommerce.Core/Entities/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ECommerce.Core.Entities
+{
+    public class OrderItem
+    {
+        public Guid ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
